8312460debf4407246db4c3d0741dbf38ff1ca44
Roman Popov <atfpom4h@gmail.com>
2026-08-05 15:40:33 +0300

    register control valve element

--- packages/process-elements/src/register.ts
+++ packages/process-elements/src/register.ts
@@ -1,5 +1,6 @@
 import { registerElementsCore } from '@pom4h/elements-core/register';
 import { registerElement } from '@pom4h/elements-core/runtime';
+import { controlValveDefinition } from './elements/control-valve.js';
 import { controllerDefinition } from './elements/controller.js';
 import { pumpDefinition } from './elements/pump.js';
 
@@ -7,6 +8,7 @@ export function registerProcessElements(): void {
   registerElementsCore();
   registerElement(pumpDefinition);
   registerElement(controllerDefinition);
+  registerElement(controlValveDefinition);
 }
 
-export { controllerDefinition, pumpDefinition };
+export { controlValveDefinition, controllerDefinition, pumpDefinition };
