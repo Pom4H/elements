d413badad99c6e1ad108cb6dd3fabd2f363aa490
Roman Popov <atfpom4h@gmail.com>
2026-08-05 16:15:14 +0300

    export control valve simulation model

--- packages/process-elements/src/index.ts
+++ packages/process-elements/src/index.ts
@@ -2,3 +2,11 @@ export { controlValveDefinition } from './elements/control-valve.js';
 export { controllerDefinition } from './elements/controller.js';
 export { pumpDefinition } from './elements/pump.js';
 export { processElementsManifest } from './manifest.js';
+export {
+  ControlValveSimulation,
+  controlValveSimulationParameters,
+  type ControlValveSimulationOptions,
+  type ControlValveSimulationParameters,
+  type ControlValveSimulationSnapshot,
+  type SimulationParameterDescriptor,
+} from './simulation/control-valve.js';
