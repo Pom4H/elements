4b7f33787af0b7467262e2a824d9ea77ec262777
Roman Popov <atfpom4h@gmail.com>
2026-08-29 01:02:03 +0300

    Export hardware wallet definition

--- packages/electrical-elements/src/index.ts
+++ packages/electrical-elements/src/index.ts
@@ -1,5 +1,6 @@
 export { breakerDefinition } from './elements/breaker.js';
 export { contactorDefinition } from './elements/contactor.js';
+export { hardwareWalletDefinition } from './elements/hardware-wallet.js';
 export { meterDefinition } from './elements/meter.js';
 export { motorDefinition } from './elements/motor.js';
 export { transformerDefinition } from './elements/transformer.js';
